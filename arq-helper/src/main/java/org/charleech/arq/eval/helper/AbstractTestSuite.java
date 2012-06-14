b6ab07c1e874facdc1e7251f552256774ff35d0b
charlee.ch <charlee.ch@CharleeCh>
2012-06-14 19:12:47 +0700

    Resolved - Issue #10: Provide the utility for removing all created archives. https://github.com/charleech/arquillian-evaluation/issues/10

--- arq-helper/src/main/java/org/charleech/arq/eval/helper/AbstractTestSuite.java
+++ arq-helper/src/main/java/org/charleech/arq/eval/helper/AbstractTestSuite.java
@@ -1,8 +1,14 @@
 package org.charleech.arq.eval.helper;
 
+import java.io.File;
+
+import lombok.extern.slf4j.Slf4j;
+
 import org.charleech.arq.eval.helper.eclipse.EclipseWrapper;
 import org.charleech.arq.eval.helper.util.JulResetter;
+import org.charleech.arq.eval.helper.util.MarkerWrapper;
 import org.junit.BeforeClass;
+import org.slf4j.Marker;
 
 /**
  * <p>
@@ -26,7 +32,15 @@ import org.junit.BeforeClass;
  *      href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative
  *      Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License</a>.
  */
+@Slf4j
 public class AbstractTestSuite {
+
+    private static final String DEFAULT_ARCHIVE_FOLDER;
+
+    static {
+        DEFAULT_ARCHIVE_FOLDER = "target/arquillian";
+    }
+
     /**
      * This is a before class configuration. The following will be performed.
      * <ul>
@@ -41,8 +55,75 @@ public class AbstractTestSuite {
      * @since 0.0.1
      */
     @BeforeClass
-    public static void before() {
+    public static void setupBeforeClass() {
         JulResetter.getINSTANCE().reset();
         EclipseWrapper.getINSTANCE().setSystemProperties();
     }
+
+    /**
+     * Delete all file from the default archive folder. The sub-folder is
+     * excluded.
+     *
+     * @param archiveFolder
+     *            The deleting file folder name
+     * @since 0.0.1
+     */
+    public static void clearCreatedArquillianArchives() {
+        AbstractTestSuite.clearCreatedArquillianArchives(
+           AbstractTestSuite.DEFAULT_ARCHIVE_FOLDER);
+    }
+
+    /**
+     * Delete all file from the specified archive folder. The sub-folder is
+     * excluded.
+     *
+     * @param archiveFolder
+     *            The deleting file folder name
+     * @since 0.0.1
+     */
+    public static void clearCreatedArquillianArchives(
+                          final String archiveFolder) {
+        File   folder = null;
+        File[] files  = null;
+        try {
+            folder = new File(archiveFolder);
+
+            if (!folder.exists()) {
+                AbstractTestSuite.log.warn(
+                   AbstractTestSuite.getMarker(),
+                   "The folder {} is not existed.",
+                   archiveFolder);
+                return;
+            }
+            if (!folder.isDirectory()) {
+                AbstractTestSuite.log.warn(
+                   AbstractTestSuite.getMarker(),
+                   "The {} is not a folder.",
+                   archiveFolder);
+                return;
+            }
+
+            files = folder.listFiles();
+            for (final File file : files) {
+                AbstractTestSuite.log.info(
+                   AbstractTestSuite.getMarker(),
+                   "The {} will be deleted",
+                   file.toString());
+                file.delete();
+            }
+        } finally {
+            folder = null;
+            files  = null;
+        }
+    }
+
+    /**
+     * Get the {@link Marker} for this class.
+     *
+     * @return The {@link Marker} for this class.
+     */
+    private static Marker getMarker() {
+        return MarkerWrapper.getINSTANCE().getMarker(AbstractTestSuite.class);
+    }
+
 }
